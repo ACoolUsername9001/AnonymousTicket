a0e57faef50acc3936819b6395485aec61c7cf24
ACoolName <acoolname9001@gmail.com>
2021-04-19 20:57:08 +0300

    changed config command order

--- cogs/configuration.py
+++ cogs/configuration.py
@@ -12,7 +12,7 @@ class Configuration(commands.Cog):
         return ctx.author.guild_permissions.administrator or (self.bot.get_data(ctx.guild.id, 'configurator_role', 0) in ctx.author.roles)
 
     @commands.guild_only()
-    @commands.command('config.set.category')
+    @commands.command('config.category.set')
     async def set_category(self, ctx, category: discord.CategoryChannel):
         if category not in ctx.guild.channels:
             await ctx.send("I can't find this category on the server")
@@ -21,7 +21,7 @@ class Configuration(commands.Cog):
         self.bot.set_data(ctx.guild.id, 'ticket_category', category.id)
         await ctx.send("Category \"{}\" has been set as the ticket category".format(category.name))
 
-    @commands.command('config.set.configurator')
+    @commands.command('config.configurator.set')
     @commands.guild_only()
     async def set_configurator_role(self, ctx, role: discord.Role):
         if role not in ctx.guild.roles:
@@ -30,7 +30,7 @@ class Configuration(commands.Cog):
         self.bot.set_data(ctx.guild.id, 'configurator_role', role.id)
         await ctx.send("Role \"{}\" has been set as the configurator role".format(role.mention))
 
-    @commands.command('config.get.category')
+    @commands.command('config.category.get')
     @commands.guild_only()
     async def get_category(self, ctx):
         category_id = self.bot.get_data(ctx.guild.id, 'ticket_category')
@@ -45,7 +45,7 @@ class Configuration(commands.Cog):
         else:
             await ctx.send("There is no ticket category yet, add one using `!config.set.category`")
 
-    @commands.command('config.get.configurator')
+    @commands.command('config.configurator.get')
     @commands.guild_only()
     async def get_configurator(self, ctx):
         role_id = self.bot.get_data(ctx.guild.id, 'configurator_role', None)
@@ -58,7 +58,7 @@ class Configuration(commands.Cog):
         else:
             await ctx.send("There is no configurator role yet, add one using `!config.set.configurator`")
 
-    @commands.command('config.get.ticket-roles')
+    @commands.command('config.ticket-roles.get')
     @commands.guild_only()
     async def get_ticket_roles(self, ctx):
         role_ids = self.bot.get_data(ctx.guild.id, 'ticket_roles_ids', [])
@@ -71,7 +71,7 @@ class Configuration(commands.Cog):
         else:
             await ctx.send("There are no ticket roles yet, add some using `!config.add.ticket_role`")
 
-    @commands.command('config.remove.ticket-roles')
+    @commands.command('config.ticket-roles.remove')
     @commands.guild_only()
     async def remove_ticket_roles(self, ctx, roles_to_remove: commands.Greedy[discord.Role]):
         roles = self.bot.get_data(ctx.guild.id, 'ticket_roles_ids', [])
@@ -80,7 +80,7 @@ class Configuration(commands.Cog):
         self.bot.set_data(ctx.guild.id, 'ticket_roles_ids', roles)
         await ctx.send("Roles \"{}\" can no longer open a ticket".format(' '.join(r.mention for r in roles_to_remove)))
 
-    @commands.command('config.add.ticket-roles')
+    @commands.command('config.ticket-roles.add')
     @commands.guild_only()
     async def add_ticket_roles(self, ctx, roles_to_add: commands.Greedy[discord.Role]):
         roles_to_add = [role for role in roles_to_add if role in ctx.guild.roles]
