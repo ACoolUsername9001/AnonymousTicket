d2b5b097a993b7f38bcdfe93b38d2ffc2c0318af
ACoolName <acoolname9001@gmail.com>
2021-04-19 15:10:02 +0300

    added include and exclude options

--- cogs/ticket.py
+++ cogs/ticket.py
@@ -3,13 +3,26 @@ import discord
 from discord.ext import commands
 from time import time
 
+
+def dms_only():
+    def predicate(ctx: commands.Context):
+        return isinstance(ctx.channel, discord.DMChannel)
+    return commands.check(predicate)
+
+
+def open_ticket_check():
+    def predicate(ctx: commands.Context):
+        return ctx.channel in ctx.bot.ticket_channels or ctx.channel in ctx.bot.ticket_channels.inv
+    return commands.check(predicate)
+
+
 class Ticket(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
 
-    def log_channel(self, channel: discord.TextChannel):
-        pass
+    # def log_channel(self, channel: discord.TextChannel):
+    #     pass
 
     @commands.command('ticket.open')
     async def ticket_open(self, ctx, *args):
@@ -37,21 +50,48 @@ class Ticket(commands.Cog):
         self.bot.ticket_channels[ctx.channel] = ticket_channel
 
     @commands.command('ticket.close')
+    @open_ticket_check()
     async def ticket_close(self, ctx):
         if isinstance(ctx.channel, discord.DMChannel):
             if ctx.channel in self.bot.ticket_channels.keys():
                 guild_channel = self.bot.ticket_channels[ctx.channel]
-                self.log_channel(guild_channel)
+                # self.log_channel(guild_channel)
                 self.bot.ticket_channels.pop(ctx.channel)
-                await guild_channel.delete()
-            else:
-                await ctx.send("I don't recall you opened a ticket")
+                await ctx.channel.send("Ticket closed")
+                # await guild_channel.delete()
         else:
             if ctx.channel not in self.bot.ticket_channels.inv:
                 return
-            self.log_channel(ctx.channel)
-            self.bot.ticket_channels.inv.pop(ctx.channel)
-            await ctx.channel.delete()
+            # self.log_channel(ctx.channel)
+            dm_channel = self.bot.ticket_channels.inv.pop(ctx.channel)
+            await dm_channel.send("Ticket closed")
+            # await ctx.channel.delete()
+
+    @commands.command('ticket.exclude')
+    @open_ticket_check()
+    async def ticket_exclude(self, ctx, members: commands.Greedy[discord.Member]):
+        if isinstance(ctx.channel, discord.DMChannel):
+            ticket_channel = self.bot.ticket_channels[ctx.channel]
+        else:
+            ticket_channel = ctx.channel
+
+        if isinstance(ticket_channel, discord.TextChannel):
+            exclude_permission = discord.PermissionOverwrite(view_channel=False)
+            for member in members:
+                await ticket_channel.set_permissions(member, overwrite=exclude_permission)
+
+    @commands.command('ticket.include')
+    @open_ticket_check()
+    async def ticket_include(self, ctx, members: commands.Greedy[discord.Member]):
+        if isinstance(ctx.channel, discord.DMChannel):
+            ticket_channel = self.bot.ticket_channels[ctx.channel]
+        else:
+            ticket_channel = ctx.channel
+
+        if isinstance(ticket_channel, discord.TextChannel):
+            include_permission = discord.PermissionOverwrite(view_channel=True)
+            for member in members:
+                await ticket_channel.set_permissions(member, overwrite=include_permission)
 
 
 def setup(bot):
